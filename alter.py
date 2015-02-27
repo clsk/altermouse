17a4ade888dd7f03e2337bfe8eeacb6090dead2a
Alan Alvarez <aalvarez@aliensoft.net>
2015-02-27 18:14:56 -0400

    introduce timer

--- alter.py
+++ alter.py
@@ -2,8 +2,8 @@ from Cocoa import *
 from Foundation import *
 from Quartz import *
 from PyObjCTools import AppHelper
+from objc import *
 from time import *
-
 
 class AppDelegate(NSObject):
     def applicationDidFinishLaunching_(self, aNotification):
@@ -11,20 +11,21 @@ class AppDelegate(NSObject):
         self.v = CGPointMake(0.0, 0.0)
         self.p = CGPointMake(0.0, 0.0)
         self.t = time()
+        self.setTimer()
 
     def setScreenBounds(self, screenBounds):
         self.screenBounds = screenBounds
 
-    def timeElapsed(t0, t1):
+    def timeElapsed(self, t0, t1):
         return t0 - t1
 
-    def calcAcceleration(delta, k=1):
+    def calcAcceleration(self, delta, k=1):
         return CGPointMake(delta.x*k, delta.y*k)
 
-    def calcVelocity(v0, a):
+    def calcVelocity(self, v0, a):
         return CGPointMake(v0.x + a.x, v0.y + a.y)
 
-    def calcPosition(p0, v, t):
+    def calcPosition(self, p0, v, t):
         return CGPointMake(p0.x + (v.x*t), p0.y + (v.y*t))
 
     def switchYOrigin(self, point):
@@ -34,10 +35,24 @@ class AppDelegate(NSObject):
         #print dir(event)
         if self.p.x == 0 and self.p.y == 0:
             self.p = self.switchYOrigin(event.locationInWindow())
-        print self.switchYOrigin(event.locationInWindow())
+        #print self.switchYOrigin(event.locationInWindow())
         #print "delta D: (%d, %d)" % (event.deltaX(), event.deltaY())
-        self.v = self.calcVelocity(self.v, self.calcAcceleration(CGPointMake(event.deltaX(), event.deltaY())))
-        #self.moveMouse(CGPointMake(event.deltaX(), event.deltaY()))
+        delta = CGPointMake(event.deltaX(), event.deltaY())
+        a = self.calcAcceleration(delta)
+        self.v = self.calcVelocity(self.v, a)
+
+    def setTimer(self):
+        sel = objc.selector(self.updateCursor,signature='v@:')
+        self.timer = NSTimer.timerWithTimeInterval_target_selector_userInfo_repeats_(0.2,self,sel,None,True)
+        NSRunLoop.currentRunLoop().addTimer_forMode_(
+                self.timer, NSDefaultRunLoopMode)
+
+    def updateCursor(self):
+        t1 = time()
+        te = self.timeElapsed(self.t, t1)
+        self.p = self.calcPosition(self.p, self.v, te)
+        self.moveMouse(self.p)
+        print 'updated cursor to ' + repr(self.p)
 
     def moveMouse(self, to):
         evsrc = CGEventSourceCreate(kCGEventSourceStateCombinedSessionState);
