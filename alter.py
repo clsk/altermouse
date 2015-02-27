751727872349979d3d40e992d08d2de032120278
Alan Alvarez <aalvarez@aliensoft.net>
2015-02-27 17:14:08 -0400

    switchYOrigin

--- alter.py
+++ alter.py
@@ -1,36 +1,55 @@
 from Cocoa import *
 from Foundation import *
+from Quartz import *
 from PyObjCTools import AppHelper
-
+from time import *
 
 
 class AppDelegate(NSObject):
     def applicationDidFinishLaunching_(self, aNotification):
         NSEvent.addGlobalMonitorForEventsMatchingMask_handler_(NSMouseMovedMask, self.mouseMoved)
-        self.v = CGPointMake(0, 0)
+        self.v = CGPointMake(0.0, 0.0)
+        self.p = CGPointMake(0.0, 0.0)
+        self.t = time()
 
-    def calcAcceleration(self, delta, k=1):
+    def setScreenBounds(self, screenBounds):
+        self.screenBounds = screenBounds
+
+    def timeElapsed(t0, t1):
+        return t0 - t1
+
+    def calcAcceleration(delta, k=1):
         return CGPointMake(delta.x*k, delta.y*k)
 
-    def updateV(self, v0, a):
+    def calcVelocity(v0, a):
         return CGPointMake(v0.x + a.x, v0.y + a.y)
 
-    def updateP(self, d0, v):
-        return CGPointMake(d0.x + v.x, d0.y + v.y)
+    def calcPosition(p0, v, t):
+        return CGPointMake(p0.x + (v.x*t), p0.y + (v.y*t))
+
+    def switchYOrigin(self, point):
+        return CGPointMake(point.x, self.screenBounds.size.height - point.y)
 
     def mouseMoved(self, event):
-        print "delta D: (%d, %d)" % (event.deltaX(), event.deltaY())
+        #print dir(event)
+        if self.p.x == 0 and self.p.y == 0:
+            self.p = self.switchYOrigin(event.locationInWindow())
+        print self.switchYOrigin(event.locationInWindow())
+        #print "delta D: (%d, %d)" % (event.deltaX(), event.deltaY())
+        self.v = self.calcVelocity(self.v, self.calcAcceleration(CGPointMake(event.deltaX(), event.deltaY())))
+        #self.moveMouse(CGPointMake(event.deltaX(), event.deltaY()))
 
-    def moveMouse(to):
-        evsrc = Quartz.CGEventSourceCreate(Quartz.kCGEventSourceStateCombinedSessionState);
-        Quartz.CGEventSourceSetLocalEventsSuppressionInterval(evsrc, 0.0);
-        Quartz.CGAssociateMouseAndMouseCursorPosition (0);
-        Quartz.CGWarpMouseCursorPosition(to);
-        Quartz.CGAssociateMouseAndMouseCursorPosition (1);
+    def moveMouse(self, to):
+        evsrc = CGEventSourceCreate(kCGEventSourceStateCombinedSessionState);
+        CGEventSourceSetLocalEventsSuppressionInterval(evsrc, 0.0);
+        CGAssociateMouseAndMouseCursorPosition (0);
+        CGWarpMouseCursorPosition(to);
+        CGAssociateMouseAndMouseCursorPosition (1);
 
 def main():
     app = NSApplication.sharedApplication()
     delegate = AppDelegate.alloc().init()
+    delegate.setScreenBounds(CGDisplayBounds(CGMainDisplayID()))
     NSApp().setDelegate_(delegate)
     AppHelper.runEventLoop()
 
